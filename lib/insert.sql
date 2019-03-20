9b14032c1944048b648c06a0cd44bac2d5ba46b1
A Glazer <avigayilglazer@gmail.com>
2019-03-20 19:22:20 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, color) VALUES ('Mr. Chocolate', 2, 'brown');
+INSERT INTO bears (name, age, color) VALUES ('Rowdy', 4, 'black');
+INSERT INTO bears (name, age, color) VALUES ('Tabitha', 2, 'black');
+INSERT INTO bears (name, age, color) VALUES ('Sergeant Brown', 3, 'white');
+INSERT INTO bears (name, age, color) VALUES ('Melissa', 7, 'white');
+INSERT INTO bears (name, age, color) VALUES ('Grinch', 5, 'cinnamon');
+INSERT INTO bears (name, age, color) VALUES ('Wendy', 1, 'blond');
+INSERT INTO bears (name, age, color) VALUES (NULL, 1, 'blond');
