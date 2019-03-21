327c23d61c5bd5462e626f33360b2317ae9ab775
dylanclements <dclements@protonmail.com>
2019-03-21 10:58:12 -0400

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Mr. Chocolate", 2, "Male", "Brown", "Choleric", 1);
-INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Rowdy", 5, "Female", "Red", "Sanguine", 1);
-INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Tabitha", 72, "Female", "White", "Melancholic", 0);
-INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Sergeant Brown", 34, "Male", "Turquoise", "Phlegmatic", 1);
-INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Melissa", 13, "Female", "Purple", "Choleric", 1);
-INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Grinch", 45, "Male", "Green", "Melancholic", 0);
-INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Wendy", 29, "Female", "Blue", "Sanguine", 1);
-INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES (NULL, NULL, "Female", "Black", "Sanguine", 1);
+INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "brown", "choleric", 1);
+INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Rowdy", 5, "female", "red", "sanguine", 1);
+INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Tabitha", 72, "female", "white", "melancholic", 0);
+INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Sergeant Brown", 34, "male", "turquoise", "phlegmatic", 1);
+INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Melissa", 13, "female", "purple", "choleric", 1);
+INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Grinch", 45, "male", "green", "melancholic", 0);
+INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES ("Wendy", 29, "female", "blue", "sanguine", 1);
+INSERT INTO bears (name, age, gender,color,temperament, alive) VALUES (NULL, NULL, "female", "black", "sanguine", 1);
