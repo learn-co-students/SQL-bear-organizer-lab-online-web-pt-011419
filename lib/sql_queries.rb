8a905f7834852ec1ff270032ad9931ca24c7cefc
dylanclements <dclements@protonmail.com>
2019-03-22 13:27:53 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   ## Last one
-  "SELECT color, COUNT(color) FROM bears"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color LIMIT 1 DESC;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
