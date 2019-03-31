e430bbc36a82c7d4f8e26df22374e612edf26f90
Nissan Holzer <nissan.holzer@gmail.com>
2019-03-31 01:05:38 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,3 +1,4 @@
+
 def selects_all_female_bears_return_name_and_age
   "SELECT name, age FROM bears WHERE gender = 'F'"
 end
@@ -19,13 +20,14 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT "
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(color) desc LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears WHERE name IS NULL"
 end
+
